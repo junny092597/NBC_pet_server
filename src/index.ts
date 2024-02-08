0100738731a730db6a8754b065f9a52d871f6c9c
junny092597 <junny092597@naver.com>
2024-02-08 13:40:16 +0900

    서버에 닉네임도 저장

--- src/index.ts
+++ src/index.ts
@@ -16,17 +16,22 @@ const io = new Server(server, {
   }
 });
 
-let onlineUsers: string[] = []; // 온라인 사용자 소켓 ID 목록을 저장할 배열, 타입을 string[]으로 명시
+// 온라인 사용자 정보를 저장할 배열, 각 객체는 소켓 ID와 닉네임을 포함
+let onlineUsers: { id: string; nickname: string; }[] = [];
 
 io.on('connection', (socket: Socket) => {
   console.log(`New client connected: ${socket.id}`);
-  onlineUsers.push(socket.id); // 사용자(소켓 ID)를 온라인 목록에 추가
-  io.emit('online users', onlineUsers); // 모든 클라이언트에게 온라인 사용자 목록을 방송
+  
+  // 새로운 연결 시 사용자 닉네임을 받기 위한 이벤트 리스너 추가
+  socket.on('user nickname', (nickname) => {
+    onlineUsers.push({ id: socket.id, nickname: nickname });
+    io.emit('online users', onlineUsers.map(user => user.nickname)); // 닉네임 목록만 방송
+  });
 
   socket.on('disconnect', () => {
     console.log(`Client disconnected: ${socket.id}`);
-    onlineUsers = onlineUsers.filter(user => user !== socket.id); // 사용자를 목록에서 제거
-    io.emit('online users', onlineUsers); // 변경된 목록을 다시 방송
+    onlineUsers = onlineUsers.filter(user => user.id !== socket.id); // 사용자를 목록에서 제거
+    io.emit('online users', onlineUsers.map(user => user.nickname)); // 변경된 목록을 다시 방송
   });
 
   socket.on('chat message', (message: any) => {
